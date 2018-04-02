f5034d2216e3d10ce2eed9187a40293007251b3f
User <User@6F-WS-088>
2018-04-02 17:25:52 +0530

    fff

--- src/test/java/CRM/Combinedformtest.java
+++ src/test/java/CRM/Combinedformtest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
-public class CombinedformTest extends Logintest{
+public class Combinedformtest extends Logintest{
 	
 
 	@Test(priority=3)
